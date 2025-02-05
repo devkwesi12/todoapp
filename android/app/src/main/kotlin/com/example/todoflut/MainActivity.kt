516bc36c19466bc2f2b7b4191b277f275da5053c
devkwesi12 <devkwasi12@gmail.com>
2025-02-05 14:40:26 +0000

    newapppush

---
+++ android/app/src/main/kotlin/com/example/todoflut/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todoflut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Rows and Columns"),
+
+      ),
+      body: MyWidget(),
+    ),
+  ));
+}
+
+class MyWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+      children: [
+        Container(width: 100,height: 100,color: Colors.purple,),
+        Container(width: 100,height: 100,color: Colors.teal,),
+        Container(width: 100,height: 100,color: Colors.green,)
+      ],
+    );
+
+  }
+}
